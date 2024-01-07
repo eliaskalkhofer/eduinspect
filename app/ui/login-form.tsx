4a8103eb65c2d5b02c93fc7c5e1824d5e96f63ea
eliaskalkhofer <elias.kalkhofer@htl-wels.at>
2024-01-07 16:25:44 +0100

    loing-form updated ui

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -2,9 +2,9 @@
 
 import { lusitana } from '@/app/ui/fonts';
 import {
-  AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
+  HashtagIcon
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
@@ -24,9 +24,9 @@ export default function LoginForm() {
           <div>
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
+              htmlFor="username"
             >
-              Email
+              Username
             </label>
             <div className="relative">
               <input
@@ -34,10 +34,10 @@ export default function LoginForm() {
                 id="username"
                 type="text"
                 name="username"
-                placeholder="Enter your acronym address"
+                placeholder="Kürzel eingeben"
                 required
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <HashtagIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           <div className="mt-4">
@@ -55,7 +55,7 @@ export default function LoginForm() {
                 name="password"
                 placeholder="Enter password"
                 required
-                minLength={4}
+                minLength={6}
               />
               <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
