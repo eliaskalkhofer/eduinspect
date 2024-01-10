573e082a24927124807af261592c631dc4d8269f
eliaskalkhofer <elias.kalkhofer@htl-wels.at>
2024-01-10 14:09:36 +0100

    Updated and seeded placeholer-data

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -83,6 +83,62 @@ const hospitations = [
 	information: "Wahrscheinlichkeitsrechnung",
 	status: "verfügbar"
 }
+,
+{
+    teacherUsername: "FELT",
+    teacherFirstname: "Tobias",
+    teacherLastname: "Fellner",
+	date: "04.01.2024",
+	starttime: "07:50",
+	endtime: "08:40",
+	subject: "AM",
+	information: "Differentialgleichung",
+	status: "verfügbar"
+},
+{
+    teacherUsername: "FELT",
+    teacherFirstname: "Tobias",
+    teacherLastname: "Fellner",
+	date: "05.01.2024",
+	starttime: "07:50",
+	endtime: "08:40",
+	subject: "AM",
+	information: "Statistik",
+	status: "verfügbar"
+},
+{
+    teacherUsername: "WINJ",
+    teacherFirstname: "Josef",
+    teacherLastname: "Winkler",
+	date: "06.01.2024",
+	starttime: "07:50",
+	endtime: "08:40",
+	subject: "INSY",
+	information: "Produktionsabläufe",
+	status: "verfügbar"
+},
+{
+    teacherUsername: "KRAM",
+    teacherFirstname: "Thomas",
+    teacherLastname: "Krammer",
+	date: "07.01.2024",
+	starttime: "07:50",
+	endtime: "08:40",
+	subject: "WIR",
+	information: "Arbeitsrecht",
+	status: "verfügbar"
+},
+{
+    teacherUsername: "FELT",
+    teacherFirstname: "Tobias",
+    teacherLastname: "Fellner",
+	date: "08.01.2024",
+	starttime: "07:50",
+	endtime: "08:40",
+	subject: "AM",
+	information: "Statistik",
+	status: "verfügbar"
+}
 ]
 
 module.exports = {
