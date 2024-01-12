531a67f1abc31d4907d4a7f9c96d579f7ded22b6
eliaskalkhofer <elias.kalkhofer@htl-wels.at>
2024-01-12 13:21:11 +0100

    Added hospitationlist functionality and accept hospitation functionality

--- app/ui/hospitationlist/table.tsx
+++ app/ui/hospitationlist/table.tsx
@@ -1,63 +1,65 @@
-import { fetchAvailableHospitations } from '@/app/lib/data';
-import { AcceptHospitation } from '@/app/ui/hospitation/buttons';
-import { getSessionUsername } from '@/app/lib/session/sessionactions';
+import { fetchAssignedHospitations } from '@/app/lib/data';
+import { getSessionUsername } from "@/app/lib/session/sessionactions";
 
 export default async function HospitationTable() {
+  
+  const username = await getSessionUsername();
+  console.log("hospitationlist---username: " + username);
+  var implementingTeacher: String;
+  var hospitations;
 
-  const hospitations = await fetchAvailableHospitations();
-  var username = "";
-  const tryUsername = await getSessionUsername();
-  if(tryUsername) {
-    username = tryUsername;
-  }
-
-  if(hospitations && hospitations.length > 0) {
-    console.log("table---Hospitationen angekommen");
+  if(username) {
+    implementingTeacher = username;
+    console.log("hospitationlist---implementingTeacher: " + implementingTeacher);
+    hospitations = await fetchAssignedHospitations(implementingTeacher);
+    if(hospitations && hospitations.length > 0) {
+      console.log("hospitationlist---Hospitationen angekommen");
+    }
   }
 
   return (
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-          <div className="md:hidden">
+        <div className="md:hidden">
 
-            {hospitations?.map((hospitation) => (
-              <div
-                key={String(hospitation._id)}
-                className="mb-2 w-full rounded-md bg-white p-4"
-              >
-                <div className="flex w-full items-center justify-between pt-4">
-                  <div>
-                    <p className="text-xl font-medium">
-                      {hospitation.teacherFirstname}
-                    </p>
-                    <p className="text-xl font-medium">
-                      {hospitation.teacherLastname}
-                    </p>
-                    <p className="text-xl font-medium">
-                      {hospitation.date}
-                    </p>
-                    <p className="text-xl font-medium">
-                      {hospitation.starttime}
-                    </p>
-                    <p className="text-xl font-medium">
-                      {hospitation.endtime}
-                    </p>
-                    <p className="text-xl font-medium">
-                      {hospitation.subject}
-                    </p>
-                    <p className="text-xl font-medium">
-                      {hospitation.information}
-                    </p>
-                  </div>
-                  <div className="">
-                    <AcceptHospitation id={String(hospitation._id)} username={username}/>
-                  </div>
-                </div>
-              </div>
-            ))}
+{hospitations?.map((hospitation) => (
+  <div
+    key={String(hospitation._id)}
+    className="mb-2 w-full rounded-md bg-white p-4"
+  >
+    <div className="flex w-full items-center justify-between pt-4">
+      <div>
+        <p className="text-xl font-medium">
+          {hospitation.teacherFirstname}
+        </p>
+        <p className="text-xl font-medium">
+          {hospitation.teacherLastname}
+        </p>
+        <p className="text-xl font-medium">
+          {hospitation.date}
+        </p>
+        <p className="text-xl font-medium">
+          {hospitation.starttime}
+        </p>
+        <p className="text-xl font-medium">
+          {hospitation.endtime}
+        </p>
+        <p className="text-xl font-medium">
+          {hospitation.subject}
+        </p>
+        <p className="text-xl font-medium">
+          {hospitation.information}
+        </p>
+      </div>
+      <div className="">
+        Button comming soon
+      </div>
+    </div>
+  </div>
+))}
 
-          </div>
+</div>
           <table className="hidden min-w-full text-gray-900 md:table">
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
@@ -95,9 +97,8 @@ export default async function HospitationTable() {
                   <td className="whitespace-nowrap px-5 py-3">-</td>
                   <td className="whitespace-nowrap px-5 py-3">{hospitation.endtime}</td>
                   <td className="whitespace-nowrap px-5 py-3">{hospitation.subject}</td>
-                  {/* Warum muss py erhöht werden, wenn das element das attribut hidden bekommt? */}
                   <td className="whitespace-nowrap px-5 py-5 hidden lg:block">{hospitation.information}</td>
-                  <td className="whitespace-nowrap px-5 py-3 text-right"><AcceptHospitation id={String(hospitation._id)} username={username} /></td>
+                  <td className="whitespace-nowrap px-5 py-3 text-right"></td>
                 </tr>
               ))}
             </tbody>
