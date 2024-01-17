ee20302febd66897a9076870746fa4d2d6ce5eb5
Tobias Mitterwallner <tobias.mitterwallner@htl-wels.at>
2024-01-17 13:36:45 +0100

    Added FAQ section and customized landing page a bit further

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import FAQSection from "./ui/landingpage/faq";
 
 export default function Page() {
   return (
@@ -8,9 +9,9 @@ export default function Page() {
     {/* <Image src="{/public/landing_page_image.webp} width={720} height={600} className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="Landing Page Picture" /> */}
     <Image src="/landing_page_image.webp" alt="something" height={512} width={512} className="rounded"/>
     <div className="text-center lg:w-2/3 w-full">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Microdosing synth tattooed vexillologist</h1>
-      <p className="mb-8 leading-relaxed">Meggings kinfolk echo park stumptown DIY, kale chips beard jianbing tousled. Chambray dreamcatcher trust fund, kitsch vice godard disrupt ramps hexagon mustache umami snackwave tilde chillwave ugh. Pour-over meditation PBR&amp;B pickled ennui celiac mlkshk freegan photo booth af fingerstache pitchfork.</p>
-      <div className="flex justify-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Willkommen bei Eduinspect!</h1>
+      <p className="mb-8 leading-relaxed">Willkommen auf unserer Hospitationswebseite, einer Plattform, die speziell für Lehrer entwickelt wurde, um wertvolle Einblicke in den Unterricht und den Unterrichtsstil ihrer Kollegen/Kolleginnen zu gewinnen. Hier haben Lehrer die Möglichkeit, ihre Kollegen im Unterricht zu beobachten und im Anschluss konstruktives Feedback zu geben.</p>
+      <div className="flex justify-center py-10">
         <Link href="/login">
           <button className="inline-flex text-white bg-orange-500 border-0 py-2 px-6 focus:outline-none hover:bg-orange-600 rounded text-lg">
             Login
@@ -22,6 +23,7 @@ export default function Page() {
           </button>
         </Link>
       </div>
+      <FAQSection />
     </div>
   </div>
 </main>
